c7ac2121264df63c1ab73035dd7e5202cb2d2e84
Покальнетов Павел Сергеевич <redx@mail.ru>
2022-05-18 16:43:40 +0500

    исходный

---
+++ chat-server/src/ru/geekbrains/jt/chat/server/core/ClientThread.java
@@ -0,0 +1,66 @@
+package ru.geekbrains.jt.chat.server.core;
+
+import ru.geekbrains.jt.chat.common.Messages;
+import ru.geekbrains.jt.network.SocketThread;
+import ru.geekbrains.jt.network.SocketThreadListener;
+
+import java.io.IOException;
+import java.net.Socket;
+
+public class ClientThread extends SocketThread {
+    private String nickname;
+    private boolean isAuthorized;
+
+    private long clientConnectionTime;
+    private boolean isReconnecting;
+
+    public ClientThread(SocketThreadListener listener, String name, Socket socket) {
+        super(listener, name, socket);
+            Runnable runnable = new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    Thread.sleep(120000);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                if (!isAuthorized) close();
+                }
+        };
+        Thread t = new Thread(runnable);
+        t.start();
+    }
+
+    public boolean isReconnecting() {
+        return isReconnecting;
+    }
+
+    public String getNickname() {
+        return nickname;
+    }
+
+    public synchronized boolean isAuthorized() {
+        return isAuthorized;
+    }
+
+    void reconnect() {
+        isReconnecting = true;
+        close();
+    }
+
+    synchronized void authAccept(String nickname) {
+        isAuthorized = true;
+        this.nickname = nickname;
+        sendMessage(Messages.getAuthAccept(nickname));
+    }
+
+    void authFail() {
+        sendMessage(Messages.getAuthDenied());
+        close();
+    }
+
+    void msgFormatError(String msg) {
+        sendMessage(Messages.getMsgFormatError(msg));
+        close();
+    }
+}
---
+++ chat-server/src/ru/geekbrains/jt/chat/server/core/SqlClient.java
@@ -0,0 +1,50 @@
+package ru.geekbrains.jt.chat.server.core;
+
+import java.sql.*;
+
+public class SqlClient {
+    private static Connection connection;
+    private static Statement statement;
+
+    synchronized static void connect() {
+        try {
+            Class.forName("org.sqlite.JDBC");
+            connection = DriverManager.getConnection("jdbc:sqlite:chat-server/db/clients-db.sqlite");
+            statement = connection.createStatement();
+        } catch (ClassNotFoundException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    synchronized static void disconnect() {
+        try {
+            connection.close();
+        } catch (SQLException throwables) {
+            throw new RuntimeException(throwables);
+        }
+    }
+
+    synchronized static String getNick(String login, String password) {
+        String query = String.format(
+                "select nickname from users where login='%s' and password='%s'",
+                login, password);
+        try (ResultSet set = statement.executeQuery(query)) {
+            if (set.next())
+                return set.getString("nickname");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        return null;
+    }
+   /* synchronized static boolean addLogin(String nikname, String login, String password){
+        /* добавление пары  login,password в базу данных
+        String query = String.format("INSERT INTO users (login,password,nickname) VALUES ('%s','%s','%s')",login,password,nickname);
+        *
+        String query = String.format(
+                "select nickname from users where login='%s'",login);
+        try (ResultSet set = statement.executeQuery(query)) {
+            if (set.next())
+
+        }
+    }*/
+}
